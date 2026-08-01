594a6a8aab5d9b73263f0a658c0b34ed00b07e97
Bent Bracke <Bent@bracke.dk>
2026-08-01 23:08:30 +0200

    Suppress intentional AUnit allocation warning

--- tests/src/awk_tests-suite.adb
+++ tests/src/awk_tests-suite.adb
@@ -936,7 +936,9 @@ package body Awk_Tests.Suite is
    function Suite return AUnit.Test_Suites.Access_Test_Suite is
       Result : constant AUnit.Test_Suites.Access_Test_Suite := new AUnit.Test_Suites.Test_Suite;
    begin
+      pragma Warnings (Off, "use of an anonymous access type allocator");
       Result.Add_Test (new CLI_Case);
+      pragma Warnings (On, "use of an anonymous access type allocator");
       return Result;
    end Suite;
 end Awk_Tests.Suite;
