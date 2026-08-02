efdd2f6cfb1726d93d2d367075dd7cd024d8f4d6
Bent Bracke <Bent@bracke.dk>
2026-08-02 19:53:25 +0200

    Localize AWK data diagnostics

--- tests/src/awk_workflows.adb
+++ tests/src/awk_workflows.adb
@@ -551,7 +551,8 @@ procedure Awk_Workflows is
             U.To_Unbounded_String (" / {option}"),
             U.To_Unbounded_String ("hint: {detail}"),
             U.To_Unbounded_String ("AWK-data"),
-            U.To_Unbounded_String ("input standard")];
+            U.To_Unbounded_String ("input standard"),
+            U.To_Unbounded_String ("AWK data")];
       begin
          for Locale_Index in 1 .. Awk_Catalog_Policy.Supported_Locale_Count loop
             declare
