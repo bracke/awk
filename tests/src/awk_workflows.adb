8701572a8b73716858c99507b1c7d9c1c2455e93
Bent Bracke <Bent@bracke.dk>
2026-08-02 19:21:21 +0200

    Correct missing option argument translations

--- tests/src/awk_workflows.adb
+++ tests/src/awk_workflows.adb
@@ -537,7 +537,8 @@ procedure Awk_Workflows is
             U.To_Unbounded_String ("standard output"),
             U.To_Unbounded_String ("is unsupported because"),
             U.To_Unbounded_String ("is reserved for AWK data"),
-            U.To_Unbounded_String ("error: {")];
+            U.To_Unbounded_String ("error: {"),
+            U.To_Unbounded_String (" / {option}")];
       begin
          for Locale_Index in 1 .. Awk_Catalog_Policy.Supported_Locale_Count loop
             declare
