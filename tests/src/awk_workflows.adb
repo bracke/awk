bfd43c5caf2b8f08b55c0e09077f086f7e9e89f4
Bent Bracke <Bent@bracke.dk>
2026-08-02 20:01:34 +0200

    Localize program file option metavars

--- tests/src/awk_workflows.adb
+++ tests/src/awk_workflows.adb
@@ -504,7 +504,8 @@ procedure Awk_Workflows is
             U.To_Unbounded_String ("input standard"),
             U.To_Unbounded_String ("error AWK"),
             U.To_Unbounded_String ("AWK error"),
-            U.To_Unbounded_String ("[operand...]")];
+            U.To_Unbounded_String ("[operand...]"),
+            U.To_Unbounded_String ("-f file, -ffile")];
       begin
          for Locale_Index in 1 .. Awk_Catalog_Policy.Supported_Locale_Count loop
             declare
