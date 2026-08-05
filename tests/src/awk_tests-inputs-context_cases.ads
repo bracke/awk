57261f1c0b6e58f38cdd430843ae31af199f56dd
Bent Bracke <Bent@bracke.dk>
2026-08-05 09:05:08 +0200

    Split input tests

---
+++ tests/src/awk_tests-inputs-context_cases.ads
@@ -0,0 +1,8 @@
+with AUnit.Test_Cases;
+
+package Awk_Tests.Inputs.Context_Cases is
+   --  In-memory invocation input and file failure cases.
+
+   procedure Register (T : in out AUnit.Test_Cases.Test_Case'Class);
+   --  @param T AUnit test case receiving context input routines.
+end Awk_Tests.Inputs.Context_Cases;
---
+++ tests/src/awk_tests-inputs-load_cases.ads
@@ -0,0 +1,8 @@
+with AUnit.Test_Cases;
+
+package Awk_Tests.Inputs.Load_Cases is
+   --  Unit-level input loader ordering and implicit-stdin cases.
+
+   procedure Register (T : in out AUnit.Test_Cases.Test_Case'Class);
+   --  @param T AUnit test case receiving input loader routines.
+end Awk_Tests.Inputs.Load_Cases;
